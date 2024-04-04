173000d1cace042929a4627f9d88ac495ecc942d
Mrrrax714 <165872819+Mrrrax714@users.noreply.github.com>
2024-04-04 02:40:56 -0700

    Update validateRemoteURL.php

--- validateRemoteURL.php
+++ validateRemoteURL.php
@@ -1,14 +1,10 @@
 function validateRemoteURL($url)
 {
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt($ch, CURLOPT_NOBODY, true);
     curl_exec($ch);
     $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-    if ($retcode != 200) {
-        $error .= "The source specified is not a valid URL.";
-    }
     curl_close($ch);
-return $error;
+    return $retcode;
 }
